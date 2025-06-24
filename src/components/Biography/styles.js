53196eb4e579538424ee2bdd7efa58456c63d9aa
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2025-06-24 09:24:07 -0300

    finalizando biografia

--- src/components/Biography/styles.js
+++ src/components/Biography/styles.js
@@ -1,37 +1,48 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0 2rem;
-  padding-top: 2.4rem;
+  padding: 2.4rem 2rem;
   background-color: var(--neutralLight);
   color: var(--neutralDark);
   h2 {
+    text-align: center;
     font-size: 2.4rem;
     color: var(--primary);
     margin-top: 2rem;
   }
-  p {
-    font-size: 1.6rem;
-    line-height: 1.6;
-    text-align: justify;
-    max-width: 800px;
-  }
-  p:first-of-type {
-    margin-top: 2rem;
-  }
-  p + p {
-    margin-top: 1.5rem;
-  }
 
-  img {
-    border-radius: 50%;
-    object-fit: cover;
+  .biography-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
     width: 100%;
-    max-width: 300px;
-    aspect-ratio: 1/1;
-    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    max-width: 1200px;
+    margin: 0 auto;
+    img {
+      margin-top: 1.6rem;
+      border-radius: 50%;
+      object-fit: cover;
+      width: 100%;
+      max-width: 400px;
+      aspect-ratio: 1/1;
+    }
+    .biography-text {
+      p {
+        font-size: 1.6rem;
+        line-height: 1.6;
+        text-align: justify;
+        max-width: 800px;
+      }
+      p + p {
+        margin-top: 1.6rem;
+      }
+    }
+    @media (min-width: 768px) {
+      flex-direction: row;
+      .biography-text {
+        max-width: 600px;
+      }
+    }
   }
 `;
